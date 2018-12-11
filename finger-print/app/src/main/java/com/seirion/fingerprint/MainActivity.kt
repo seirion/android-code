8bd4c1b34276f395c4349c006149c856c910b0de
babo <seirion@mensakorea.org>
2018-12-11 10:01:51 +0900

    finger print sample code

--- finger-print/app/src/main/java/com/seirion/fingerprint/MainActivity.kt
+++ finger-print/app/src/main/java/com/seirion/fingerprint/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.seirion.fingerprint
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.github.ajalt.reprint.core.AuthenticationResult
+import com.github.ajalt.reprint.core.Reprint
+import com.github.ajalt.reprint.rxjava2.RxReprint
+
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Reprint.initialize(this)
+        initUi()
+    }
+
+    private fun initUi() {
+        if (Reprint.isHardwarePresent() && Reprint.hasFingerprintRegistered()) {
+            Log.d(TAG, "finger print ready")
+            button.setOnClickListener { startBiometrics() }
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun startBiometrics() {
+        Log.d(TAG, "startBiometrics()")
+        text.text = "Listening"
+        RxReprint.authenticate()
+            .subscribe({ res ->
+            when (res.status) {
+                AuthenticationResult.Status.SUCCESS -> text.text = "SUCCESS"
+                AuthenticationResult.Status.NONFATAL_FAILURE -> text.text = "NONFATAL_FAILURE"
+                AuthenticationResult.Status.FATAL_FAILURE -> text.text = "NONFATAL_FAILURE"
+            }
+        }, { Log.d(TAG, "failed : $it")})
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
     }
 }
